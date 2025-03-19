abc8aa1cad14e93d74a8d04385d7898dcfcc0770
Cameron Banowsky <cameron@shebash.io>
2025-03-19 11:05:57 -0700

    Fix temp flag handling in send command

--- test/temp-encrypted.test.js
+++ test/temp-encrypted.test.js
@@ -38,4 +38,40 @@ describe('Temporary Encrypted Pastes Integration Tests', () => {
     // This is a documentation/placeholder test
     expect(true).to.be.true;
   });
+  
+  it('should create and delete temp encrypted paste from file using send command', () => {
+    // This test must be run manually:
+    // 1. Create a test file:
+    //    echo "Secret file content" > /tmp/test-file.txt
+    // 2. Create an encrypted temporary paste from the file:
+    //    dedpaste send --file /tmp/test-file.txt --encrypt --temp
+    // 3. View the paste once:
+    //    dedpaste get <paste-url>
+    // 4. Attempt to view it again (should fail with 404):
+    //    dedpaste get <paste-url>
+    
+    console.log(`
+    Manual test instructions for send command with file:
+    
+    1. Create a test file:
+       echo "Secret file content" > /tmp/test-file.txt
+    
+    2. Create an encrypted temporary paste from the file:
+       dedpaste send --file /tmp/test-file.txt --encrypt --temp
+    
+    3. The command will output a URL like: https://paste.d3d.dev/e/AbCdEfGh
+    
+    4. Retrieve and decrypt the paste:
+       dedpaste get https://paste.d3d.dev/e/AbCdEfGh
+    
+    5. Try to retrieve the same paste again:
+       dedpaste get https://paste.d3d.dev/e/AbCdEfGh
+    
+    6. Verify you receive a 404 response on the second attempt,
+       confirming the paste was properly deleted after first view.
+    `);
+    
+    // This is a documentation/placeholder test
+    expect(true).to.be.true;
+  });
 });
